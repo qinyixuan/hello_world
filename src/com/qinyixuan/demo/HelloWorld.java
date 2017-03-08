78f170a2050fe0d2c194e63f61536e88ecbedcd0
qinyixuan <1141569944@qq.com>
2017-03-09 00:40:49 +0800

    first

---
+++ src/com/qinyixuan/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.qinyixuan.demo;
+
+/**
+ * Created by QinYiXuan on 2017/3/8.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
