6dc1bcc567046c572598768359ff012bc272cd37
qinyixuan <1141569944@qq.com>
2017-03-09 16:47:01 +0800

    add hemlDemo

--- src/com/qinyixuan/demo/HelloWorld.java
+++ src/com/qinyixuan/demo/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.qinyixuan.demo;
 
+import java.text.SimpleDateFormat;
+
 /**
  * Created by QinYiXuan on 2017/3/8.
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd:HH:mm:ss");
+        System.out.println(simpleDateFormat.format(System.currentTimeMillis()));
     }
 }
